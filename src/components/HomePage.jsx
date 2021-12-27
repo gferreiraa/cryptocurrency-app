1d99f65cfdfd7189fc919f84a135d6ae1b816f9a
Getúlio Rafael Ferreira <gferreiraa@gmail.com>
2021-12-26 23:04:56 -0300

    FEATURE: ADD Redux Toolkit | Created queries requests

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/crypoApi';
 
 const HomePage = () => {
+
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+    console.log("globalStats", globalStats)
+
+    if(isFetching) return 'Loading...';
+
+    console.log(data)
+
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <Typography.Title level={2} className="heading"> Global Crypto Stats</Typography.Title>
+            <Row>
+                <Col span={12}>
+                    <Statistic title="Total Cryptocurrencies" value={globalStats.total}/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Exchanges" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Market Cap" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total 24h Volume" value="5"/>
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Markets" value="5"/>
+                </Col>
+            </Row>
+        </>
     )
 }
 
